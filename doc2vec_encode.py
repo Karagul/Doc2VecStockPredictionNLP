07365a2c66bbe3b474982efb86311c723b30bb7e
= <zykigix@hotmail.com>
2018-11-25 15:35:18 +0700

    clean code

--- doc2vec_encode.py
+++ doc2vec_encode.py
@@ -1,71 +1,74 @@
-import pandas as pd 
-import gensim 
+import pandas as pd
+import gensim
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 
 def open_csv():
     """
-    Read news data from all CSV files and append them together. 
+    Read news data from all CSV files and append them together.
     """
-    df = pd.DataFrame(columns=['time','headline','assetCodes','sentimentNegative','sentimentNeutral','sentimentPositive'])
+    df = pd.DataFrame(columns=['time', 'headline', 'assetCodes',
+                               'sentimentNegative', 'sentimentNeutral',
+                               'sentimentPositive'])
     file_count = 10
     for i in range(0,file_count):
-        i += 1 
-        if i < 10 : 
+        i += 1
+        if i < 10 :
             file_name = "data/news/news_data_"+str(0)+str(i)+".csv"
-        else : 
+        else :
             file_name = "data/news/news_data_"+str(i)+".csv"
         print("Open : " + file_name)
         load_df = pd.read_csv(file_name)
-        load_df = load_df[[['time','headline','assetCodes','sentimentNegative','sentimentNeutral','sentimentPositive']]]
+        load_df = load_df[[['time', 'headline', 'assetCodes',
+                            'sentimentNegative', 'sentimentNeutral',
+                            'sentimentPositive']]]
         df = df.append(load_df)
     return df
 
 def sector_ric():
     """
-    Read sector symbol 
+    Read sector symbol
     """
     df = pd.read_csv("data/tech_sector_ric.csv")
     ric = df["RIC"].tolist()
-    return ric 
+    return ric
 
 def clean_headline(text_list):
     """
-    Pre-processing the data 
-    1. cut stock quotes 
+    Pre-processing the data
+    1. cut stock quotes
     2. cut special alphabets
-    3. change to lower 
+    3. change to lower
 
-    input variable : text_list --> list/series/array of string 
-    input type : list/series/array-like 
-    return list/series/array-like of cleaning data 
+    input variable : text_list --> list/series/array of string
+    input type : list/series/array-like
+    return list/series/array-like of cleaning data
     """
-    return text_list 
+    return text_list
 
 def encode_to_doc2vec(model, text_list, verbose=False):
     """
-    Encode document 
+    Encode document
 
-    input variable : text_list --> list/series/array of string 
-    input type : list/series/array-like 
-    return 2d list/array 
+    input variable : text_list --> list/series/array of string
+    input type : list/series/array-like
+    return 2d list/array
     """
-    doc_vector = [] 
-    for paragraph in text_list : 
+    doc_vector = []
+    for paragraph in text_list :
         vec = model.infer_vector(paragraph)
         doc_vector.append(vec)
-        if verbose : 
+        if verbose :
             print("Encode : " + paragraph + " to ", vec)
-    return doc_vector 
+    return doc_vector
 
-def select_data(df, ric):   
+def select_data(df, ric):
     """
-    Select only used data 
+    Select only used data
     """
-    return df 
+    return df
 
 if __name__ == "__main__":
-    model= Doc2Vec.load("d2v_60.model")  
+    model= Doc2Vec.load("d2v_60.model")
     news_df = open_csv()
     news_df.headline = clean_headline(news_df.headline)
-    
--- doc2vec_training.py
+++ doc2vec_training.py
@@ -7,14 +7,15 @@ Created on Fri Nov 23 16:08:38 2018
 
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
-import pandas as pd 
+import pandas as pd
 
-def train_doc2vec(model, tagged_data, max_epochs=100, update_vocab=False, print_detail=False, save_sampling=10):
+def train_doc2vec(model, tagged_data, max_epochs=100, update_vocab=False,
+                  print_detail=False, save_sampling=10):
     print("Building vocab")
     model.build_vocab(tagged_data, update=update_vocab)
     print("Built vocab done")
     for epoch in range(max_epochs):
-        if print_detail : 
+        if print_detail :
             print('epoch = {0}'.format(epoch))
         model.train(tagged_data,
                     total_examples=model.corpus_count,
@@ -23,40 +24,45 @@ def train_doc2vec(model, tagged_data, max_epochs=100, update_vocab=False, print_
         model.alpha -= 0.0002
         # fix the learning rate, no decay
         model.min_alpha = model.alpha
-        if (epoch+1)%save_sampling == 0 :     
+        if (epoch+1)%save_sampling == 0 :
             model.save("weight/d2v_"+str(epoch+1)+".model")
     model.save("weight/d2v_final.model")
     print("Model Trained Saved ....")
     print("================  Training Done ! ===================")
-    return model 
+    return model
 
 def tag_document(document_list, print_sampling = 1,verbose=True):
-    tagged_doc = [] 
+    tagged_doc = []
     print("Tagging Document .../ ")
     print("This should take few minutes depend on your data size")
     num_doc = len(doc_list)
     for i, doc in enumerate(document_list):
-        #change to lower case and tag document 
-        tagged_doc.append(TaggedDocument(words=word_tokenize(doc.lower()), tags=[str(i)]))
-        if i%print_sampling == 0 and verbose : 
-            print("Tags : ", i ,"/", num_doc,"  Text : " + doc)    
+        #change to lower case and tag document
+        tagged_doc.append(
+            TaggedDocument(words=word_tokenize(doc.lower()), tags=[str(i)])
+        )
+        if i%print_sampling == 0 and verbose :
+            print("Tags : ", i ,"/", num_doc,"  Text : " + doc)
     return tagged_doc
 
-print("Read Cleaned Document !")
-news_train_df = pd.read_csv("data/news/news_Cut.csv")
-doc_list = news_train_df['headline'].dropna().drop_duplicates().tolist()
-max_epochs = 100
-vec_size = 20
-alpha = 0.025
-doc2vec_model = Doc2Vec(vector_size=vec_size,
-                alpha=alpha, 
-                min_alpha=0.00025,
-                min_count=1,
-                dm =1)
-print("Clear momories before training")
+if __name__ == '__main__':
+    print("Read Cleaned Document !")
+    news_train_df = pd.read_csv("data/news/news_Cut.csv")
+    doc_list = news_train_df['headline'].dropna().drop_duplicates().tolist()
+    max_epochs = 100
+    vec_size = 20
+    alpha = 0.025
+    doc2vec_model = Doc2Vec(vector_size=vec_size,
+                            alpha=alpha,
+                            min_alpha=0.00025,
+                            min_count=1,
+                            dm =1)
+    print("Clear momories before training")
 
-tagged_doc = tag_document(doc_list, print_sampling=50000)
+    tagged_doc = tag_document(doc_list, print_sampling=50000)
 
-# Clear RAM before training 
-news_train_df, doc_list = None, None 
-doc2vec_model = train_doc2vec(doc2vec_model, tagged_doc, max_epochs, False, True)
+    # Clear RAM before training
+    news_train_df, doc_list = None, None
+    doc2vec_model = train_doc2vec(
+        doc2vec_model, tagged_doc, max_epochs, False, True
+    )
